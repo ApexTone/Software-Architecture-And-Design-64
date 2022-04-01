6ba43fa584ee4a20173b0bb447bd2ebcf02e6444
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2022-04-01 15:35:41 +0700

    Lab10 - Observer

--- Lab10/behavioral1/NewsSubscriber.java
+++ Lab10/behavioral1/NewsSubscriber.java
@@ -4,5 +4,30 @@ import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
 
 public class NewsSubscriber implements Subscriber<News> {
-	// You need to implement this class
+    private final String name;
+
+    public NewsSubscriber(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        System.out.printf("%s subscribed to %s\n", name, subscription.getClass().getName());
+    }
+
+    @Override
+    public void onNext(News item) {
+        System.out.printf("%s get the news (%s): %s\n", name, item.getClass().getName(), item.getContent());
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        //throw error
+    }
+
+    @Override
+    public void onComplete() {
+        //Method invoked when it is known that no additional Subscriber method invocations will occur for a Subscription that is not already terminated by error, after which no other Subscriber methods are invoked by the Subscription.
+    }
+    // You need to implement this class
 }
